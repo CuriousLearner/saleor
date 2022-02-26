4de4455ec91e2c0bd61daa7920e30c066099acc6
Sanyam Khurana <sanyam.khurana01@gmail.com>
2022-02-27 03:44:29 +0530

    load .env from manage.py

--- manage.py
+++ manage.py
@@ -5,6 +5,17 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saleor.settings")
 
+    try:
+        from decouple import RepositoryEnv
+        file_path = os.path.normpath(os.path.join(os.path.dirname(__file__), ".."))
+        print("{}/.env".format(file_path))
+        for k,v in RepositoryEnv("{}/.env".format(file_path)).data.items():
+            # print(k, v)
+            os.environ[k] = v
+    except Exception as e:
+        print(e)
+        pass
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
